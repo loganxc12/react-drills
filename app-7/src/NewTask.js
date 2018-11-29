e7dfe14986e60a56491494d6ab71f7aa0bb92e3c
Logan Marshall <wildmovementco@gmail.com>
2018-11-29 06:42:51 -0700

    completed react app 7

---
+++ app-7/src/NewTask.js
@@ -0,0 +1,37 @@
+import React, {Component} from "react";
+
+class NewTask extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            input: ""
+        }
+        
+        this.handleAdd = this.handleAdd.bind(this);
+    }
+
+    handleInputChange(val) {
+        this.setState( {input: val});
+    }
+
+    handleAdd() {
+        this.props.add(this.state.input);
+        this.setState( {input: ""});
+    }
+
+    render() {
+        return (
+            <div>
+                <input value={ this.state.input }
+                    placeholder="Enter new task"
+                    onChange={ e => this.handleInputChange(e.target.value) }
+                />
+                <button onClick={ this.handleAdd }>Add</button>
+            </div>
+        )
+    }
+}
+
+export default NewTask;
